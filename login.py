48fa982065c2b91aaaf9191adc9f5106304532eb
deznoff <savvatretyakov@gmail.com>
2024-08-26 14:57:24 +0100

    Created login.py to retrieve the APIC login cookie

--- login.py
+++ login.py
@@ -1,2 +1,40 @@
-import pprint
+from re import S
 import requests
+import os
+import subprocess
+import sys
+import getpass
+import json
+
+usr = input('Enter your username: ')
+print('Username: ', usr)
+pwd = getpass.getpass(prompt = 'Enter your password: ', stream = None)
+
+def get_token():
+    url = 'https://x.x.x.x/api/aaaLogin.json'
+
+    payload = {
+        'aaaUser': {
+            'attributes': {
+                'name':'%s' % usr,
+                'pwd':'%s' % pwd
+            }
+        }
+    }
+
+    headers = {
+        'Content-Type' : 'application/json'
+    }
+
+    requests.packages.urllib3.disable_warnings()
+    response = requests.post(url, data = json.dumps(payload), headers = headers, verify = False).json()
+
+    token = response['imdata'][0]['aaaLogin']['attributes']['token']
+    return token
+
+def main():
+    token = get_token()
+    print('The token is:', token)
+
+if __name__ == '__main__':
+    main()
